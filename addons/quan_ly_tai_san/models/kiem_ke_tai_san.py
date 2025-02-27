35c3b5fed93f552eb6db031dae92e6d8d9b7d8fd
Nguyen Ngoc Dan Truong <dnu.dantruong@gmail.com>
2025-02-27 20:33:17 +0700

    add luan_chuyen_tai_san and its view, logic handle

--- addons/quan_ly_tai_san/models/kiem_ke_tai_san.py
+++ addons/quan_ly_tai_san/models/kiem_ke_tai_san.py
@@ -9,13 +9,22 @@ class KiemKeTaiSan(models.Model):
 
     ma_phieu_kiem_ke = fields.Char('Mã phiếu', required=True)
     ten_phieu_kiem_ke = fields.Char('Tên phiếu', required=True)
-    phong_ban_id = fields.Many2one('phong_ban', string='Bộ phận muốn kiểm kê', required=True)
+    phong_ban_id = fields.Many2one('phong_ban', string='Bộ phận cần kiểm kê', required=True)
     nhan_vien_kiem_ke_id = fields.Many2one('nhan_vien', string='Nhân viên kiểm kê', required=True)
     ds_kiem_ke_ids = fields.One2many(comodel_name='kiem_ke_tai_san_line', 
                                      inverse_name='kiem_ke_tai_san_id', 
                                      string ='Danh sách kiểm kê')
     thoi_gian_tao = fields.Datetime('Thời gian tạo phiếu', default=fields.Datetime.now)
     ghi_chu = fields.Char('Ghi chú', default='')
+    trang_thai_phieu = fields.Char(compute='_compute_trang_thai_phieu', string='Trạng thái phiếu', store=True)
+    
+    @api.depends('ds_kiem_ke_ids.trang_thai')
+    def _compute_trang_thai_phieu(self):
+        for rec in self:
+            if rec.ds_kiem_ke_ids and all(kiem_ke.trang_thai == 'finished' for kiem_ke in rec.ds_kiem_ke_ids):
+                rec.trang_thai_phieu = 'Đã kiểm kê'
+            else:
+                rec.trang_thai_phieu = 'Chưa kiểm kê'
 
     @api.onchange('phong_ban_id')
     def _onchange_phong_ban_id(self):
