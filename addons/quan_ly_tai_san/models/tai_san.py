becec2658a9b3607c7590569f75a877e6bd40c78
Nguyen Ngoc Dan Truong <dnu.dantruong@gmail.com>
2025-02-27 14:39:52 +0700

    thêm khấu hao tự động

--- addons/quan_ly_tai_san/models/tai_san.py
+++ addons/quan_ly_tai_san/models/tai_san.py
@@ -1,4 +1,6 @@
 from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+from datetime import datetime
 
 class TaiSan(models.Model):
     _name = 'tai_san'
@@ -24,6 +26,14 @@ class TaiSan(models.Model):
         ('degressive', 'Giảm dần'),
         ('none', 'Không')
     ], string='Phương pháp khấu hao', default = 'none', required=True)
+    thoi_gian_su_dung = fields.Integer('Thời gian đã sử dụng (năm)', default=0)
+
+    # Khấu hao tuyến tính
+    thoi_gian_toi_da = fields.Integer('Thời gian sử dụng còn lại tối đa (năm)', default=5)
+
+    # Khấu hao giảm dần
+    ty_le_khau_hao = fields.Float('Tỷ lệ khấu hao (%)', default=20)
+
     don_vi_tinh = fields.Char('Đơn vị tính')
     ghi_chu = fields.Char('Ghi chú')
 
@@ -35,3 +45,48 @@ class TaiSan(models.Model):
 
     phong_ban_su_dung_ids = fields.One2many('phan_bo_tai_san', 'tai_san_id', string='Phòng ban sử dụng')
     lich_su_khau_hao_ids = fields.One2many('lich_su_khau_hao', 'ma_ts', string='Lịch sử khấu hao')
+
+    def action_tinh_khau_hao(self):
+        for record in self:
+            if record.gia_tri_hien_tai <= 0:
+                raise ValidationError("Giá trị hiện tại phải lớn hơn 0 !")
+            if record.pp_khau_hao == 'none':
+                raise ValidationError("Tài sản này không có phương pháp khấu hao!")
+
+            so_tien_khau_hao = 0
+
+            if record.pp_khau_hao == 'straight-line':  
+                if record.thoi_gian_toi_da <= 0:
+                    raise ValidationError("Thời gian sử dụng tối đa phải lớn hơn 0 (năm) !")
+                so_tien_khau_hao = record.gia_tri_ban_dau / record.thoi_gian_toi_da  
+
+            elif record.pp_khau_hao == 'degressive':  
+                if record.ty_le_khau_hao <= 0 or record.ty_le_khau_hao >= 100:
+                    raise ValidationError("Tỷ lệ khấu hao phải nằm trong khoảng (0,100) !")
+                so_tien_khau_hao = record.gia_tri_hien_tai * (record.ty_le_khau_hao / 100) 
+
+            so_tien_khau_hao = min(so_tien_khau_hao, record.gia_tri_hien_tai)  
+            ma_phieu_khau_hao = 'KH' + record.ma_tai_san + '-' + datetime.now().strftime('%Y%m%d%H%M%S%f')
+
+
+            self.env['lich_su_khau_hao'].create({
+                'ma_phieu_khau_hao': ma_phieu_khau_hao,
+                'ma_ts': record.id,
+                'ngay_khau_hao': fields.Date.today(),
+                'so_tien_khau_hao': so_tien_khau_hao,
+                'gia_tri_con_lai': record.gia_tri_hien_tai,
+                'loai_phieu': 'automatic',
+                'ghi_chu': f'Khấu hao tự động ngày {fields.Date.today()}'
+            })
+
+            self.env['bus.bus']._sendone(
+                self.env.user.partner_id, 
+                'simple_notification', 
+                {
+                    'title': 'Thành công',
+                    'message': f'Khấu hao tài sản "{record.ten_tai_san}" thành công!',
+                    'sticky': False,  
+                    'type': 'success'  
+                }
+            )
+
